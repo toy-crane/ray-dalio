6fe3f02acaf6293dd6e56f83f00326339aea18c2
toycrane <alwaysfun2183@gmail.com>
2020-06-05 17:09:11 +0900

    홈 페이지 버튼 중앙 정렬

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "./Button";
 import { useHistory } from "react-router-dom";
-import { Heading, Box, Image, Text } from "rebass";
+import { Heading, Box, Image, Text, Flex } from "rebass";
 
 export const Home = () => {
 	let history = useHistory();
@@ -12,25 +12,28 @@ export const Home = () => {
 				color="primary"
 				fontWeight="bold"
 				textAlign="center"
+				lineHeight="heading"
 			>
 				나와 가장 잘 맞는 투자자 찾기 <span>🤓</span>
 			</Heading>
-			<Text fontSize={[1]} my={2}>
+			<Text fontSize={[1]} my={1} lineHeight="body">
 				워렌 버핏, 레이 달리오, 손정의 등 우리 주변에는 각자만의 색깔로
-				투자를 하는 유명한 투자자들이 있습니다.
-				<br />이 중에 당신은 어떤 투자자에 가장 가까운지 알아볼까요??
+				투자를 하는 유명한 투자자들이 있습니다. 이 중에 당신은 어떤
+				투자자에 가장 가까운지 알아볼까요??
 			</Text>
 			<Image
-				my={2}
+				my={1}
 				src="https://i.pinimg.com/originals/bb/15/28/bb1528ffb469515a407e81438e9eab9d.jpg"
 				sx={{
 					borderRadius: 8,
 				}}
 				opacity={0.8}
 			/>
-			<Button onClick={() => history.push("/quizzes")} width={1 / 2}>
-				나와 맞는 투자자 찾아보기
-			</Button>
+			<Flex justifyContent="center">
+				<Button onClick={() => history.push("/quizzes")} width={3 / 4}>
+					나와 맞는 투자자 찾아보기
+				</Button>
+			</Flex>
 		</Box>
 	);
 };
