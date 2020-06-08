e6e4a09586d280ea8ea8a49bf2a411f5ff912635
toycrane <alwaysfun2183@gmail.com>
2020-06-08 17:12:38 +0900

    페이스북 페이지 iframe으로 붙이기

--- src/Components/Result.js
+++ src/Components/Result.js
@@ -7,7 +7,22 @@ export const Result = () => {
 	} = useContext(PageContext);
 	return (
 		<div>
-			결과 페이지입니다. {risk_score} {period_score}
+			<div>
+				결과 페이지입니다. {risk_score} {period_score}
+			</div>
+			<div>
+				<iframe
+					title="facebook"
+					src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fusaant%2F&tabs=timeline&width=500&height=70&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=false&appId=575536876504340"
+					width="500"
+					height="70"
+					style={{ border: "none", overflow: "hidden" }}
+					scrolling="no"
+					frameborder="0"
+					allowTransparency="true"
+					allow="encrypted-media"
+				></iframe>
+			</div>
 		</div>
 	);
 };
