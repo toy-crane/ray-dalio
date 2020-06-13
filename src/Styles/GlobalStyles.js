6e63eeae9c34eda8ec28e630e262791dfa0d5e2d
toycrane <alwaysfun2183@gmail.com>
2020-06-13 19:21:45 +0900

    reset -> nomalize로 변경

--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -1,19 +1,21 @@
 import { css } from "@emotion/core";
-import { sm, md } from "../Styles/BreakPoints";
-import emotionReset from "emotion-reset";
+import emotionNormalize from "emotion-normalize";
 
 export default css`
 	@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap");
-	${emotionReset}
-	*, *::after, *::before {
+	${emotionNormalize}
+	html,
+    body {
+		font-family: "Noto Sans KR";
+	}
+	*,
+	*::after,
+	*::before {
 		box-sizing: border-box;
 		-moz-osx-font-smoothing: grayscale;
 		-webkit-font-smoothing: antialiased;
 	}
 	button:focus {
 		outline: 0;
-	}
-	body {
-		font-family: "Noto Sans KR";
 	}
 `;
