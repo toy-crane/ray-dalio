4868f9a132fed7c1eb01711b2f6f5faef8c798a2
toycrane <alwaysfun2183@gmail.com>
2020-07-01 16:57:12 +0900

    로딩 페이지 이미지 수정

--- src/Components/LoadingPage.js
+++ src/Components/LoadingPage.js
@@ -13,14 +13,23 @@ export const LoadingPage = () => {
 	useEffect(() => {
 		const timer = setTimeout(() => {
 			history.push(`/result/${url}`);
-		}, 4000);
+		}, 5000);
 		return () => clearTimeout(timer);
 	}, [history, url]);
 
 	return (
 		<>
-			<Header mb={3}>투자 성향 분석중...</Header>
-			<Flex justifyContent="center" mx={[5, 0]}>
+			<Header mb={3} mt={3 * 2}>
+				투자 성향 분석중...
+			</Header>
+			<Flex
+				justifyContent="center"
+				sx={{
+					margin: "0 auto",
+				}}
+				height={["200px", "auto"]}
+				width={["200px", "auto"]}
+			>
 				<Image src="/loading.gif" alt="loading..." />
 			</Flex>
 		</>
