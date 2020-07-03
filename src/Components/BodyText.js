114d8359a826e045399cab118ee40bd300bc7841
toycrane <alwaysfun2183@gmail.com>
2020-07-03 17:55:15 +0900

    더 보기 버튼 추가

--- src/Components/BodyText.js
+++ src/Components/BodyText.js
@@ -1,11 +1,32 @@
 import React from "react";
 import Text from "./Text";
 import { Flex } from "rebass";
+import ShowMore from "react-show-more";
+import { useTheme } from "emotion-theming";
+
+const customCSS = (theme) => ({
+	"a:visited": {
+		color: theme.colors.primary,
+		"text-decoration": "none",
+	},
+	"a:link": {
+		color: theme.colors.primary,
+		"text-decoration": "none",
+	},
+	"a:hover": {
+		color: theme.colors.deepGray,
+	},
+});
 
 export const BodyText = (props) => {
+	const theme = useTheme();
 	return (
-		<Flex {...props} px={[1, 2]} py={1}>
-			<Text>{props.text}</Text>
+		<Flex px={[1, 2]} py={1}>
+			<Text css={() => customCSS(theme)}>
+				<ShowMore lines={2} more="더 알아보기" less="접기">
+					{props.text}
+				</ShowMore>
+			</Text>
 		</Flex>
 	);
 };
